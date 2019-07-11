4972a40ed4f1badf23c56d317f1d9722e69e10e4
sumedh.s <sumedh.s@somaiya.edu>
2019-07-11 21:20:56 +0530

    Code commit for Nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -4,16 +4,12 @@ import { Link } from "react-router-dom";
 class Nav extends Component {
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: "orange", height: 40 }}>
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+          </li>
         </nav>
       </div>
     );
