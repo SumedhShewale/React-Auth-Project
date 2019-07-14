d1fd1c6227f5cf3573ae045f063c9d5290792d85
sumedh.s <sumedh.s@somaiya.edu>
2019-07-15 00:35:36 +0530

    server api created

--- src/Profile.js
+++ src/Profile.js
@@ -29,7 +29,7 @@ class Profile extends Component {
         <img
           style={{ height: 50, width: 50 }}
           src={profile.picture}
-          alt="profile picture"
+          alt="profile"
         />
         <pre>{JSON.stringify(profile, null, 2)}</pre>
       </div>
